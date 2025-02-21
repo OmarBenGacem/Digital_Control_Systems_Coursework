a6ad7790c1c45211f4ae9ffdeabc0838cda7c8c1
OmarBenGacem <omar.ben-gacem20@imperial.ac.uk>
2025-02-21 14:27:51 +0000

    asda

--- model/state_update.m
+++ model/state_update.m
@@ -5,7 +5,7 @@ function x_dot = state_update(x, u)
     
 
     phi_dot  = x(4);
-    phi_ddot = ( g/L + (F/(m * L) ) * x(1) - u/L ) * sin(x(3));
+    phi_ddot = ( g/L + (F/(m * L) ) * x(1) ) * sin(x(3)) - - u/L * cos(x(3));
 
     x_dot = [s_dot ; s_ddot ; phi_dot, phi_ddot];
 
