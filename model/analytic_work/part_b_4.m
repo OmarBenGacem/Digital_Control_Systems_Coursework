de9ee41cbdee8d47dbe438f64946a97ca185f451
omarbengacem <omar.ben-gacem20@imperial.ac.uk>
2025-03-21 10:37:01 +0000

    transfer function

--- model/analytic_work/part_b_4.m
+++ model/analytic_work/part_b_4.m
@@ -25,8 +25,8 @@ Kd = sym("Kd", [1,4]);
 %% Functions
 xd = symfun(sym("xd"), k); % digital representation of x
 x = symfun(sym("x", [4,1]), t);
-% dx = diff(x(t), t);
-dx = symfun(sym("dx", [4,1]), t);
+dx = diff(x(t), t);
+% dx = symfun(sym("dx", [4,1]), t);
 u = symfun(sym("u"), t);
 ud = symfun(sym("ud"), k);
 
@@ -37,23 +37,13 @@ ud = symfun(sym("ud"), k);
 
 lin_sys = dx == A * x + B * u;
 
-cntrld_sys = dx == A * x + B * K * x;
-
 
 %% DELETE - Is Repeated
-% A_val_sym = subs(A, [F, g, M, L], [F_val, g_val, M_val, L_val]);
-% B_val_sym = subs(B, [M, L], [M_val, L_val]);
-
-% A_cl = subs(A, [F, g, M, L], [F_val, g_val, M_val, L_val]) + subs(B, [M, L], [M_val, L_val]) * subs(K, K, K_val)
-
+A_val_sym = subs(A, [F, g, M, L], [F_val, g_val, M_val, L_val]);
+B_val_sym = subs(B, [M, L], [M_val, L_val]);
 T = sym("T");
 Ad = sym("Ad", size(A));
 Bd = sym("Bd", size(B));
 
-F = A - B * Kd;
-% s = sym("s", [1, 4]);
-% lap = laplace(F);
-% eigz = diag(s) - F;
-% inverse = inv(eigz);
-% invlap = ilaplace(inverse)
-
+tf = (diag([s,s,s,s]) - A) * B;
+toOverleaf(tf, "tf", true);
